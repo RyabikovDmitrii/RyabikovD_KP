a7614b1228090e76b9a1791ad204a3a29b18c12b
student251 <ryabikov.dmitrii@gmail.com>
2024-12-03 12:42:23 +0300

    Close #4 Добавлена функция "Восстановление БД"

--- yslada/InportData.cs
+++ yslada/InportData.cs
@@ -59,18 +59,15 @@ namespace yslada
 
             try
             {
-                // Обновляем строку подключения, чтобы использовать кодировку utf8mb4
                 string connectionStringWithCharset = $"{connectionString};charset=utf8mb4";
 
                 using (MySqlConnection con = new MySqlConnection(connectionStringWithCharset))
                 {
                     con.Open();
-
-                    // Используем StreamReader с кодировкой UTF-8
                     using (StreamReader reader = new StreamReader(filePath, Encoding.UTF8))
                     {
                         string line;
-                        reader.ReadLine(); // Пропускаем заголовок
+                        reader.ReadLine(); 
                         int columnCount = GetColumnCount(con, tableName);
 
                         while ((line = reader.ReadLine()) != null)
@@ -82,7 +79,6 @@ namespace yslada
                                 return;
                             }
 
-                            // Формируем команду INSERT с обработкой кавычек
                             var insertCommand = new MySqlCommand($"INSERT INTO `{tableName}` VALUES ({string.Join(",", values.Select(v => $"'{v.Replace("'", "''")}'"))});", con);
                             insertCommand.ExecuteNonQuery();
                             importedRecordsCount++;
@@ -114,28 +110,136 @@ namespace yslada
 
         private void recoverBtn_Click(object sender, EventArgs e)
         {
-            //using (MySqlConnection con = new MySqlConnection(connectionString))
-            //{
-            //    con.Open();
-            //    try
-            //    {
-            //        string createSchemaScript = @"
-  
-            //";
+            using (MySqlConnection con = new MySqlConnection(connectionString))
+            {
+                con.Open();
+                try
+                {
+                    string createSchemaScript = @"
+                      USE `yslada`;
 
-            //        using (MySqlCommand command = new MySqlCommand(createSchemaScript, con))
-            //        {
-            //            command.ExecuteNonQuery();
-            //        }
+                    --
+                    -- Table structure for table `category`
+                    --
 
-            //        MessageBox.Show("Структура базы данных восстановлена успешно.");
-            //        LoadTableNames();
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        MessageBox.Show($"Ошибка восстановления структуры базы данных: {ex.Message}");
-            //    }
-            //}
+                    DROP TABLE IF EXISTS `category`;
+                    CREATE TABLE `category` (
+                      `categoryID` int NOT NULL AUTO_INCREMENT,
+                      `name` text NOT NULL,
+                      PRIMARY KEY (`categoryID`)
+                    ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+
+                    --
+                    -- Table structure for table `menu`
+                    --
+
+                    DROP TABLE IF EXISTS `menu`;
+                    CREATE TABLE `menu` (
+                      `menuID` int NOT NULL AUTO_INCREMENT,
+                      `name` text NOT NULL,
+                      `description` text NOT NULL,
+                      `category` int NOT NULL,
+                      `image` text,
+                      `status` varchar(15) CHARACTER SET armscii8 COLLATE armscii8_general_ci DEFAULT NULL,
+                      `cost` int NOT NULL,
+                      PRIMARY KEY (`menuID`,`category`),
+                      KEY `fk_menu_category1_idx` (`category`),
+                      CONSTRAINT `fk_menu_category1` FOREIGN KEY (`category`) REFERENCES `yslada`.`category` (`categoryID`)
+                    ) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb3;
+
+                    --
+                    -- Table structure for table `order`
+                    --
+
+                    DROP TABLE IF EXISTS `order`;
+                    CREATE TABLE `order` (
+                      `orderID` int NOT NULL AUTO_INCREMENT,
+                      `tableNum` varchar(15) NOT NULL,
+                      `date` datetime NOT NULL,
+                      `costOrder` int NOT NULL,
+                      `statusOrder` text NOT NULL,
+                      `employee` text NOT NULL,
+                      `orderItemID` int DEFAULT NULL,
+                      PRIMARY KEY (`orderID`)
+                    ) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb3;
+
+                    --
+                    -- Table structure for table `orderitem`
+                    --
+
+                    DROP TABLE IF EXISTS `orderitem`;
+                    CREATE TABLE `orderitem` (
+                      `orderItemID` int NOT NULL AUTO_INCREMENT,
+                      `name` text NOT NULL,
+                      `count` int NOT NULL,
+                      PRIMARY KEY (`orderItemID`)
+                    ) ENGINE=InnoDB AUTO_INCREMENT=305 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+                    --
+                    -- Table structure for table `orderitem_has_order`
+                    --
+
+                    DROP TABLE IF EXISTS `orderitem_has_order`;
+                    CREATE TABLE `orderitem_has_order` (
+                      `orderitem_orderItemID` int NOT NULL,
+                      `order_orderID` int NOT NULL,
+                      PRIMARY KEY (`orderitem_orderItemID`,`order_orderID`),
+                      KEY `fk_orderitem_has_order_order1_idx` (`order_orderID`),
+                      KEY `fk_orderitem_has_order_orderitem1_idx` (`orderitem_orderItemID`),
+                      CONSTRAINT `fk_orderitem_has_order_order1` FOREIGN KEY (`order_orderID`) REFERENCES `order` (`orderID`) ON DELETE CASCADE ON UPDATE CASCADE,
+                      CONSTRAINT `fk_orderitem_has_order_orderitem1` FOREIGN KEY (`orderitem_orderItemID`) REFERENCES `orderitem` (`orderItemID`) ON DELETE CASCADE ON UPDATE CASCADE
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+                    --
+                    -- Table structure for table `role`
+                    --
+
+                    DROP TABLE IF EXISTS `role`;
+                    CREATE TABLE `role` (
+                      `roleID` int NOT NULL AUTO_INCREMENT,
+                      `name` text NOT NULL,
+                      PRIMARY KEY (`roleID`)
+                    ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+
+                    --
+                    -- Table structure for table `user`
+                    --
+
+                    DROP TABLE IF EXISTS `user`;
+                    CREATE TABLE `user` (
+                      `userID` int NOT NULL AUTO_INCREMENT,
+                      `surname` text NOT NULL,
+                      `name` text NOT NULL,
+                      `patronymic` text NOT NULL,
+                      `number` varchar(12) NOT NULL,
+                      `userLogin` text NOT NULL,
+                      `userPasswd` text NOT NULL,
+                      `role` int NOT NULL,
+                      PRIMARY KEY (`userID`,`role`),
+                      KEY `fk_user_role1_idx` (`role`),
+                      CONSTRAINT `fk_user_role1` FOREIGN KEY (`role`) REFERENCES `yslada`.`role` (`roleID`)
+                    ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
+            ";
+
+                    using (MySqlCommand command = new MySqlCommand(createSchemaScript, con))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+
+                    MessageBox.Show("Структура базы данных восстановлена успешно.");
+                    DialogResult res = MessageBox.Show("Хотите ли добавить данные в таблицы?", "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (res == DialogResult.No)
+                    {
+                        Close();
+                    }
+
+                    LoadTableNames();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка восстановления структуры базы данных: {ex.Message}");
+                }
+            }
         }
     }
 }
